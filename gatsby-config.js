3eb4be3b68f82fb641872f3e261711bc9d6f8ea7
ZihaoShu <202502016@njnu.edu.cn>
2023-04-22 13:56:35 +0800

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,106 @@
+module.exports = {
+  pathPrefix: process.env.PATH_PREFIX || '/',
+  siteMetadata: {
+    siteTitle: 'Workouts Map',
+    siteUrl: 'https://ben29.xyz',
+    logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
+    description: 'Personal Workouts Page',
+    keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
+    navLinks: [
+      {
+        name: 'Blog',
+        url: 'https://github.com/ZihaoShu',
+      },
+      {
+        name: 'About',
+        url: 'https://github.com/ZihaoShu',
+      },
+    ],
+  },
+  plugins: [
+    'gatsby-transformer-json',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/static/',
+      },
+    },
+    {
+      resolve: "gatsby-plugin-vercel",
+      options: {
+        // (optional) Prints metrics in the console when true
+        debug: false,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/images/',
+      },
+    },
+    {
+      resolve: 'gatsby-alias-imports',
+      options: {
+        rootFolder: './',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        cssLoaderOptions: {
+          esModule: false,
+          modules: {
+            namedExport: false,
+          },
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#e1e1e1',
+        theme_color: '#e1e1e1',
+        display: 'standalone',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https?:.*\/icons\/.*\.png/,
+            handler: `CacheFirst`,
+          },
+          {
+            urlPattern:
+              /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+            handler: `StaleWhileRevalidate`,
+          },
+          {
+            urlPattern: /^https?:\/\/api\.mapbox\.com\//,
+            handler: `StaleWhileRevalidate`,
+          },
+          {
+            urlPattern: /^https?:.*\/page-data\/.*\.json/,
+            handler: `StaleWhileRevalidate`,
+          },
+        ],
+        skipWaiting: true,
+        clientsClaim: true,
+      },
+    },
+  ],
+};
